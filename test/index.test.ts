17b60794d65efa8c3206bc62335dd51e8dd97973
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2022-07-07 15:52:34 +0200

    chore: Use personal eslint-config to update to ESLint 8 (#57)
    Additionally, this patch gets rid of eslint-plugin-jsdoc, which is
    honestly unneeded for a project this size.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 import { PassThrough } from 'stream'
 
-import WritableWrapper from '../index'
+import WritableWrapper from '../index.js'
 
 describe('WritableWrapper', function () {
   describe('#write()', function () {
